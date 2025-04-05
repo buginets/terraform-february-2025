69f91ad165c75cd4af83b0fdd907e0c6228efdaa
buginets <adibaigaziev@gmail.com>
2025-04-05 18:06:47 +0000

    Class3 added

---
+++ Class3-part2/key.tf
@@ -0,0 +1,19 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+
+  tags = {
+    Name = "Hello"
+    Team = "DevOps"
+    
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "kaizen-adiletba"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
